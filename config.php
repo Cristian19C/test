70afcca8d355ebda9aa6b8ab643b6dcb9cd71189
Cristian19C <cristian.cordoba01@uptc.edu.co>
2025-04-11 22:42:49 -0500

    feat: agregar variables de entorno para la conexion a la base de datos

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
 // config.php
-$host = '';
-$dbname = '';
-$user = '';
-$pass = '';
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
